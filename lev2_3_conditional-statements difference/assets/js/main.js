626154b86d85d089a64d511bf9a852e373cd607c
codesfromannywhere <codesfromannywhere>
2023-02-21 15:35:10 +0100

    update

--- lev2_3_conditional-statements difference/assets/js/main.js
+++ lev2_3_conditional-statements difference/assets/js/main.js
@@ -2,31 +2,23 @@ console.log("läuft");
 
 const input = document.querySelector('form input[type="number"]');
 
-function calc(num) {
-    let diff = num - 27;
-    if (diff > 27) {
+function calc() {
+    let diff = input.value - 27;
+    if (diff >= 27) {
         console.log("multipliziere", diff * 2);
     }
 }
 
-calc(35);
-calc(74);
-calc(123);
 
-
-// function calc() {
-//     let diff = input.value - 27;
-//     if (diff > 27) {
+// function calc(num) {
+//     let diff = num - 27;
+//     if (diff >= 27) {
 //         console.log("multipliziere", diff * 2);
 //     }
 // }
 
-// const num = 27;
-// console.log(input.value - num)
+// calc(35);
+// calc(74);
+// calc(123);
 
-// function calc() {
-//     if (input.value > - 27); {
-//     let diff = input.value - 27
-//     console.log("multipliziere", diff * 2)
-//     }
-// }
+
